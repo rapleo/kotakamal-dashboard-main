2b067b328aa8bbffbdb463e9c338811ad2f0a6d0
RapLeo <rifqiariopratama@gmail.com>
2021-01-13 17:48:14 +0700

    Tambah menu lagi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          menu baru ngab!
+          menu baru ngab
         </p>
       </header>
     </div>
