abdfb1c609358fd33fa5e5694b1eb6e32ebfa806
RapLeo <rifqiariopratama@gmail.com>
2021-01-13 17:42:54 +0700

    Menu baru nih sab

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          menu baru ngab!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
